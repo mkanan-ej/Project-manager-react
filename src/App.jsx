db3fc6049aa7e0bee2672885fbec1efe592e3e92
Mohammad Kanaan <mkanaan@ejada.com>
2025-11-20 16:54:05 +0300

    added some classes for styles

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import ProjectSidebar from "./components/ProjectSidebar";
 
 function App() {
   return (
-    <main>
+    <main className="h-screen my-8 flex gap-8">
       <ProjectSidebar />
     </main>
   );
--- src/components/ProjectSidebar.jsx
+++ src/components/ProjectSidebar.jsx
@@ -1,3 +1,10 @@
 export default function ProjectSidebar(){
-    return()
+    return(
+        <aside>
+            <h2>Your projects</h2>
+            <div><button className="px-4 py-2 text-xs md:text-base rounded-md bg-stone-700 text-stone-400 hover:bg-stone-600 hover:text-stone-100">+ Add project</button></div>
+            <ul></ul>
+        </aside>
+
+    );
 }
